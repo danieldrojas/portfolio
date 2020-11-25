6cce3f55df12cc88c2be705a7998d7fcfe026e6c
Daniel Rojas <daniel.drojas@outlook.com>
2020-11-25 17:14:22 -0500

    Updates

---
+++ precache-manifest.297eb03f3ccacd8216b0acabfb4be43c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca6cac7562305e960db8c78967dc8ed1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8afb5cd2b241b55adf22",
+    "url": "/portfolio/static/css/main.f91adf82.chunk.css"
+  },
+  {
+    "revision": "53007a4d6721a84145cd",
+    "url": "/portfolio/static/js/2.9b9f599a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/portfolio/static/js/2.9b9f599a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8afb5cd2b241b55adf22",
+    "url": "/portfolio/static/js/main.c321ca52.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "c49bd555160e6abdcc91e26e3347757d",
+    "url": "/portfolio/static/media/bootCampConnect.c49bd555.png"
+  },
+  {
+    "revision": "4d73bc5459a8f7fa8f3f45169dddda2f",
+    "url": "/portfolio/static/media/fixedphoto.4d73bc54.png"
+  },
+  {
+    "revision": "cab40242c8a1def20be11a4584b4c403",
+    "url": "/portfolio/static/media/quiz.cab40242.png"
+  }
+]);
